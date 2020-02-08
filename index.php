6f80b0aae1d0c209265564088ca9489dd91c2a97
anwarnasihin <anwarnasihin555@gmail.com>
2020-02-08 23:48:47 +0700

    menambhan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello Word!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. A excepturi est, aliquid autem nihil quaerat enim. Hic, sunt animi, fugiat et qui excepturi est, natus maxime sequi totam eveniet blanditiis.</p>
 </body>
 </html>
